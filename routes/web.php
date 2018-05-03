dba8180674cbaf99db09a795fb70f14e0e4c1e9b
WeerakoonOS <osw0294@gmail.com>
2018-05-03 12:35:49 +0530

    google sign in route added

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('googlesign', function () {
+    return view('/auth/googlesign');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
