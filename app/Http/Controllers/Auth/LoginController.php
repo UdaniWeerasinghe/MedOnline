349a4ca62d99d9c59dda41446142c6c2fbf25f75
WeerakoonOS <osw0294@gmail.com>
2018-05-03 13:29:26 +0530

    google API credentials

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-
+use App\User;
+use Socialite;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -32,6 +34,52 @@ class LoginController extends Controller
      *
      * @return void
      */
+
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToGoogleProvider()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleGoogleProviderCallback()
+    {
+        try{
+            $user = Socialite::driver('google')->user();
+        }catch (Exception $e) {
+            return redirect('login/google');
+        }
+
+        $authUser = $this->CreateUser($user);
+        Auth::login($authUser, true);
+        return redirect()->route('home');
+    }
+
+    public function CreateUser($user)
+    {
+        $authUser = User::where('email', $user->email)->first();
+        if ($authUser) {
+            return $authUser;
+
+        }
+        return User::create([
+            'name'     => $user->name,
+            'email'    => $user->email,
+        ]);
+    }
+
+    public function showLoginForm(){
+        return view('auth.login')->withTitle('Login');
+    }
+
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
