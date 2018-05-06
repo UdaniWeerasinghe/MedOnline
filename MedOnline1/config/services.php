8818cbd0297cba8a5736309735fa61508e554db7
WeerakoonOS <osw0294@gmail.com>
2018-05-06 10:11:23 +0530

    use this folder

---
+++ MedOnline1/config/services.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+    ],
+
+    'ses' => [
+        'key' => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => 'us-east-1',
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe' => [
+        'model' => App\User::class,
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+
+    'google' => [
+        'client_id' => '772511268573-eik2k7v78mghu6vj65q08852k0avl2k2.apps.googleusercontent.com',
+        'client_secret' => 'VtZzurjLrTwJnkt8quEQ0yhl',
+        'redirect' => 'http://localhost:8000/login/google/callback',
+    ],
+
+];
