461e055ef197afafe46fe83ab12dab2056c34d95
WeerakoonOS <osw0294@gmail.com>
2018-05-06 16:33:27 +0530

    nav links

--- MedOnline1/resources/views/welcome.blade.php
+++ MedOnline1/resources/views/welcome.blade.php
@@ -71,8 +71,11 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
+                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/about') }}">About</a>
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
+
                     @endauth
                 </div>
             @endif
