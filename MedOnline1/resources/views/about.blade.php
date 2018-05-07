ca0117fe53cd1e444634ca158a514f344b1f1d41
WeerakoonOS <osw0294@gmail.com>
2018-05-07 16:12:45 +0530

    about page

--- MedOnline1/resources/views/about.blade.php
+++ MedOnline1/resources/views/about.blade.php
@@ -1,66 +1,37 @@
-@extends('layouts.app')
+<html>
+<body> 
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">MedOnline</div>
+<!-- @section('content') -->
+    <div class="container">
+        <header>@extends('layouts.app')</header>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">MedOnline</div>
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <!-- <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
+                    <div class="panel-body">
+                        <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <div class="col-md-8 col-md-offset-4">
+                                    <h2>What is MedOnline ?</h2>
+                                    <p>We are a dedicated group to make the lives easier for
+                                    people who are busy to take care of them! We provide home-delivery
+                                    service of your own prescription with quality supply of medicines from
+                                    50+ suppliers both from overseas and local. </p>
                                 </div>
                             </div>
-                        </div> -->
-
-
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <h2>What is MedOnline ?</h2>
-                                <p></p>
-                            </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+    <!-- @endsection -->
+    <div>
+    <!-- <footer class = 'row'>
+        @include('includes.footer')
+    </footer> -->
+    </div>
+</body>
+</html>
