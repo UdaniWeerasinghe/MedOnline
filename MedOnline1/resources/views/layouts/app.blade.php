43070a631f52d7d26ddc4fdb3f95309b7dd03157
WeerakoonOS <osw0294@gmail.com>
2018-05-07 16:13:10 +0530

    routes added

--- MedOnline1/resources/views/layouts/app.blade.php
+++ MedOnline1/resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'MedOnline') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -29,7 +29,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        {{ config('app.name', 'MedOnline') }}
                     </a>
                 </div>
 
@@ -49,6 +49,9 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+                            <li><a href="{{ url('/home') }}">Home</a></li>
+                            <li><a href="{{ url('/about') }}">About</a></li>
+                            <li><a href="{{ url('/contact') }}">Contact us </a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
--- MedOnline1/routes/web.php
+++ MedOnline1/routes/web.php
@@ -19,6 +19,10 @@ Route::get('/about', function (){
     return view('about');
 });
 
+Route::get('/contact', function (){
+    return view('contact');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
