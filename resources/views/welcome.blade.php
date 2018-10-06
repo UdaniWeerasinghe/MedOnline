cd5c40ab64d63b3f30a5739d7fa55ae026cd8876
Jayendra Weerasinghe <sachiejayen@gmail.com>
2018-10-06 23:58:43 +0530

    Change front end logins

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,8 +71,8 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <a href="{{ route('login') }}">Sign In</a>
+                        <a href="{{ route('register') }}">Sign Up</a>
                     @endauth
                 </div>
             @endif
